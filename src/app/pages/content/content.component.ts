89869ae8e20bbf0f5966ed233d354cef83defff9
thalisson <thalissonboth9@gmail.com>
2023-09-29 22:26:06 -0300

    alterações nas rotas

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-content',
@@ -9,9 +10,13 @@ export class ContentComponent implements OnInit{
   photoCover:string = "https://animes.olanerd.com/wp-content/uploads/2023/08/1691039930_Revelada-a-sinopse-da-ultima-temporada-de-Desencanto-1200x900.jpg"
   contentTitle:string = "NOTICIA EXEMPLO"
   contentDescription:string = "Bla bla bla blabla "
-  constructor() {  }
+  
+  constructor(
+    private route:ActivatedRoute
+  ) {  }
 
   ngOnInit(): void {
-    
+    this.route.paramMap.subscribe( value => console.log(value.get("id"))
+    )
   }
 }
