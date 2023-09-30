97f7246d2116060a01673b2da65d4f65056cd1b4
thalisson <thalissonboth9@gmail.com>
2023-09-30 12:45:39 -0300

    content created

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,26 @@
 export const dataFake = [
     {
-        "id": 1,
-        "photo": "aaa",
-        "title": "Saiu novo filme",
-        "description": "ola"
+        "id": '1',
+        "title": "Game of Thrones",
+        "description": "Da melhor série para a pior série tudo isso em 8 temporadas de muito amor e odio. ",
+        "photoCover": "https://s2.glbimg.com/lip1rXjb7IfKWKhzwjKCCsKJcrA=/e.glbimg.com/og/ed/f/original/2017/08/17/game-of-thrones.jpg"
     },
     {
-        "id": 2,
-        "photo": "aaa",
-        "title": "Saiu novo filme",
-        "description": "ola"
+        "id": '2',
+        "title": "Modern Family",
+        "description": "Uma das melhores séries",
+        "photoCover": "https://images-na.ssl-images-amazon.com/images/S/pv-target-images/8e85a0db8f33b66cf292717a5dddfa5a79c4daa59916521470df71240bd6fb60._RI_TTW_.jpg"
+    },
+    {
+        "id": '3',
+        "title": "(Des)encanto",
+        "description": "Animação estilo simpsons que alegrou meu coração",
+        "photoCover": "https://www.cinemascomics.com/wp-content/uploads/2018/08/desencanto-wallpaper.jpg"
+    },
+    {
+        "id": '4',
+        "title": "The Officie",
+        "description": "Considerava a pior série durante a primeira temporada porem virou uma das minhas favoritas",
+        "photoCover": "https://flxt.tmsimg.com/assets/p185008_b_h9_ai.jpg"
     }
 ];
